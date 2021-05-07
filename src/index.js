253adaefc42a96c93a14676a5d50d1cb26df5630
IgorSukovsky <Sukovsky70@gmail.com>
2021-05-07 17:08:58 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './styles.css';
+// console.log('arr');
+import './js/test';
+
+//import print from './js/test';   defaulet
+
+//console.log(print);
+
+//print('not text');
+
+// import { text, printText } from './js/test';
+
+import * as object from './js/test';
+
+console.log(object.text);
+
+object.printText('J - S');
---
+++ src/js/test.js
@@ -0,0 +1,20 @@
+import menu from '../menu.json';
+
+//export const text = 'Text texs';
+//export function printText(string) {
+//console.log(string);
+//}
+
+//export default printText;
+const text = 'Text texs';
+function printText(string) {
+  console.log(string);
+}
+printText(text);
+
+console.log(menu);
+
+const data = {};
+console.log(data);
+
+export { text, printText, data };
